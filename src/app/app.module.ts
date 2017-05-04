f70729c9fcb6e11a1d951907fda91d808e61a0b3
Michael Purdy <0mpurdy@gmail.com>
2017-05-04 08:59:51 +0100

    Add routing framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+
+export const appRoutes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { LandingComponent } from './landing/landing.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
