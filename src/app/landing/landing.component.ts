9a4bee694ae0059d10c9eea5df8fc3d966660f35
Michael Purdy <0mpurdy@gmail.com>
2017-05-04 11:02:13 +0100

    Add websocket connection
    Fix roomForm not initialised
    Add angular material theme

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -9,15 +9,27 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LandingComponent implements OnInit {
 
   roomForm: FormGroup;
-  room: string;
 
   constructor(private fb: FormBuilder) {
-    this.fb.group({
-      roomName: this.fb.control('')
+    this.roomForm = this.fb.group({
+      roomName: this.fb.control(''),
+      handle: this.fb.control('')
     });
   }
 
   ngOnInit() {
+  }
+
+  submit(formValue) {
+    console.log(formValue);
+    const url = 'wss://quiz-monster.herokuapp.com/chat/' + formValue.roomName + '/' + formValue.handle + '/';
+    console.log(url);
+    const websocket = new WebSocket(url);
+    websocket.onmessage = (message) => {
+      // console.log(message);
+      const messageData = JSON.parse(message.data);
+      console.log(messageData);
+    };
   }
 
 }
