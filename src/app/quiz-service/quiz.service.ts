a6bc12962edc24e9bae184bab73ed1d7458cc876
Michael Purdy <0mpurdy@gmail.com>
2017-05-04 12:54:50 +0100

    Add reconnecting web socket

--- src/app/quiz-service/quiz.service.ts
+++ src/app/quiz-service/quiz.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+declare var ReconnectingWebSocket;
+
 @Injectable()
 export class QuizService {
 
@@ -12,11 +14,17 @@ export class QuizService {
     this.currentHandle = roomDetails.handle;
     const url = 'wss://quiz-monster.herokuapp.com/quiz/' + roomDetails.roomName + '/' + roomDetails.handle + '/';
     console.log(url);
-    this.websocket = new WebSocket(url);
+    this.websocket = new ReconnectingWebSocket(url);
+    this.websocket.onopen = () => {
+      console.log('websocket opened');
+    };
     this.websocket.onmessage = (message) => {
       // console.log(message);
       const messageData = JSON.parse(message.data);
       console.log('Got new message', messageData);
+    };
+    this.websocket.onclose = () => {
+      console.log('websocket closed');
     };
 
     setTimeout(() => {
